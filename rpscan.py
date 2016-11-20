f675a4a889a0419d4ed21301187f7b4b8abfe61a
DavidJea <djea@mistsys.com>
2016-11-20 04:44:05 -0800

    add write_samples_to_file

--- rpscan.py
+++ rpscan.py
@@ -21,6 +21,10 @@ import bluetooth._bluetooth as bluez
 
 e = threading.Event()
 
+pktTemplate = {"hashkey":"","Protocol":"TCP","Beacon":[],"Accelerometer":[],"topic":"mobile-client-staging","isDeviceMoving":false,"SDK":"2.7.0","Device":{"Model":"Android","UniqueID":""},"V":1,"Date":""}
+
+ORGID = "6748cfa6-4e12-11e6-9188-0242ac110007"
+
 # rpscan class scans for 1 minute, and write results into a file 
 class rpscan(threading.Thread):
     def __init__(self):
@@ -51,13 +55,32 @@ class rpscan(threading.Thread):
         blescan.hci_enable_le_scan(sock)
         t1 = time.time()
         while True:
-            pkts.extend(blescan.parse_events(sock, 10))
+            pkts.extend(blescan.parse_events(sock, 10))  #parse needed packets in blescan
             if time.time - t1 > 60:
                 break
         blescan.hci_disable_le_scan(sock)
         return pkts
         
     def write_samples_to_file(self,pkts):
+        sorted_pkts = sorted(pkts, key=lambda x: x['TS'])
+        t = int(sorted_pkts[0][TS])
+        samples_this_second = {}
+        f = open("mysamples.txt","w")
+        for p in sorted_pkts:
+            if p['TS'] > t:
+                # a new second
+                for minor,rssi in samples_this_second.iteritems():
+                   pktTemplate["Beacon"] += [{{"RSSIs":rssi,"UUID":ORGID,"Minor":minor,"Major":"0","TxPower":100}}]
+                f.write(json.dumps(pktTemplate)+"\n")
+                samples_this_second = {}
+                t = int(p['TS'])
+            else:
+                samples_this_second[p['MINOR']] += [p['RSSI']]
+        # last second
+        for minor,rssi in samples_this_second.iteritems():
+           pktTemplate["Beacon"] += [{{"RSSIs":rssi,"UUID":ORGID,"Minor":minor,"Major":"0","TxPower":100}}]
+        f.write(json.dumps(pktTemplate)+"\n")
+        f.close()
         return
 
 
